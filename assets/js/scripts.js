ac3bf14f6e657d89ea327a8945aa7d510827eeac
Rijon <rijon.r@yahoo.com>
2020-11-01 22:29:04 +0600

    Commit done

---
+++ assets/js/scripts.js
@@ -0,0 +1,135 @@
+window.addEventListener('DOMContentLoaded', () => {
+  fetch('api/read-channels.php')
+    .then(res => res.json())
+    .then(data => checkChannelCookieAndRender(data))
+    .catch(err => console.log(err))
+})
+
+async function checkChannelCookieAndRender(channels) {
+  let channelCookieStatuses = new Array(channels.length)
+  let iCount = 0
+  channels.data.forEach(async channel => {
+    await fetch('api/check-channel-cookie.php', {
+      method: 'POST',
+      body: JSON.stringify({
+        channel_id: channel.id
+      })
+    })
+      .then(res => res.json())
+      .then(data => {
+        channelCookieStatuses[channel.id - 1] = data.status
+        if (++iCount == 15) renderChannels(channels, channelCookieStatuses)
+      })
+      .catch(err => console.log(err))
+  })
+}
+
+
+
+async function renderChannels(channels, cookieStatuses) {
+  channels.data.forEach((channel, index) => {
+    let ratingNANCheck = 'N/R'
+    let ratingContainer = ''
+    if (channel.rating) ratingNANCheck = `${Math.round(channel.rating * 10) / 10}/5`
+    if (cookieStatuses[index] == 'unset') {
+      ratingContainer = `
+                <div class="give">
+                    <i class="fas fa-star give-rating"></i>
+                    <i class="fas fa-star give-rating"></i>
+                    <i class="fas fa-star give-rating"></i>
+                    <i class="fas fa-star give-rating"></i>
+                    <i class="fas fa-star give-rating"></i>
+                </div>
+            `
+    }
+    const parser = new DOMParser();
+    const parsedDocument = parser.parseFromString(`
+          <div class="channel">
+            <span class="avg-rating">${ratingNANCheck}</span>
+            <img class="img"  src=${channel.logo_url} />
+            <a href=${channel.channel_url} class="title">${channel.title}</a>
+            ${ratingContainer}
+          </div>`, "text/html");
+    document.getElementById('channels').appendChild(parsedDocument.getElementsByTagName('div')[0], true);
+  })
+  starCounter();
+}
+
+function starCounter() {
+  document.querySelectorAll('.channel').forEach((starContainer, index) => {
+    starContainer.addEventListener('click', (e) => {
+      let count = 1
+      if (e.target.tagName == 'path' || e.target.tagName == 'svg') {
+        if (e.target.tagName == 'path') e.target.parentElement.parentElement.id = 'clicked'
+        if (e.target.tagName == 'svg') e.target.parentElement.id = 'clicked'
+        let ratingCount = e.target.closest('div.channel').querySelector('span.avg-rating')
+        console.log(ratingCount)
+        let currentStar = e.target.parentElement
+        while (currentStar.previousElementSibling) {
+          currentStar = currentStar.previousElementSibling
+          ++count
+        }
+        createChannelCookie(index, count, ratingCount)
+      } else {
+        console.log(e.target.tagName)
+      }
+    })
+  })
+}
+
+
+function createChannelCookie(channelId, rating, ratingCount) {
+  fetch('api/create-channel-cookie.php', {
+    method: 'POST',
+    body: JSON.stringify({
+      channel_id: channelId + 1
+    })
+  })
+    .then(res => res.json())
+    .then(data => {
+      createNewRating(channelId, rating, ratingCount)
+    })
+    .catch(err => console.log(err))
+}
+
+
+
+function createNewRating(channelId, rating, ratingCount) {
+  fetch('api/create-rating.php', {
+    method: 'POST',
+    body: JSON.stringify({
+      channel_id: channelId + 1,
+      rating: rating
+    })
+  })
+    .then(res => res.json())
+    .then(data => {
+      if (data.status === 'Rating Inserted') {
+        document.querySelector('#clicked').style.visibility = 'hidden';
+        document.querySelector('#clicked').id = '';
+        updateNewRating(channelId, ratingCount);
+      }
+
+    })
+    .catch(err => console.log(err))
+}
+
+
+
+function updateNewRating(channelId, ratingCount) {
+  fetch('api/read-single-channel.php', {
+    method: 'POST',
+    body: JSON.stringify({
+      channel_id: channelId + 1
+    })
+  })
+    .then(res => res.json())
+    .then(data => {
+      if (data.status === 'Channel Found') {
+        let newRating = data.data.rating;
+        ratingCount.innerText = `${Math.round(newRating * 10) / 10}/5`
+      }
+
+    })
+    .catch(err => console.log(err))
+}
---
+++ your-favourite-youtube-channel.sql
@@ -0,0 +1,157 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 12, 2020 at 05:30 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `your-favourite-youtube-channel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `channel`
+--
+
+CREATE TABLE `channel` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `channel_url` varchar(255) NOT NULL,
+  `logo_url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `channel`
+--
+
+INSERT INTO `channel` (`id`, `title`, `channel_url`, `logo_url`) VALUES
+(1, 'freeCodeCamp.org', 'https://www.youtube.com/channel/UC8butISFwT-Wl7EV0hUK0BQ', 'https://yt3.ggpht.com/a/AATXAJwFt03RAznOsPwlfo5c1kW1rp-1o3Xgpw9MNreQMQ=s100-c-k-c0xffffffff-no-rj-mo'),
+(2, 'Moshiur', 'https://www.youtube.com/channel/UCcPGGA2RWx6C5o1xl18vUWA', 'https://yt3.ggpht.com/a/AATXAJxCr4YUcVVqqdEwrOowHmCvbjKLzARti4U0kqiB=s100-c-k-c0xffffffff-no-rj-mo'),
+(3, 'Dark Code', 'https://www.youtube.com/channel/UCD3KVjbb7aq2OiOffuungzw', 'https://yt3.ggpht.com/a/AATXAJwFdgGg8C4SE-tLg1ZOF3HVid9o32Gnp8QNSX1StQ=s100-c-k-c0xffffffff-no-rj-mo'),
+(4, 'Julio Codes', 'https://www.youtube.com/channel/UCeHWTyAKBv_WnW8gsEKGECw', 'https://yt3.ggpht.com/a/AATXAJwNGcbFGSpAbaIsonsLtd7lOjYOCF5EFkkYqTNjBQ=s100-c-k-c0xffffffff-no-rj-mo'),
+(5, 'Online Tutorials', 'https://www.youtube.com/channel/UCbwXnUipZsLfUckBPsC7Jog', 'https://yt3.ggpht.com/a/AATXAJwBtKw7erVLtS31HvYvlsqSZ5tPti-kxeJNjEebiQ=s100-c-k-c0xffffffff-no-rj-mo'),
+(6, 'Web Dev Simplified', 'https://www.youtube.com/channel/UCFbNIlppjAuEX4znoulh0Cw', 'https://yt3.ggpht.com/a/AATXAJzlhhukYsdgVeuIspbug5SNo_254oqP9ZyrY2E0=s100-c-k-c0xffffffff-no-rj-mo'),
+(7, 'whatsdev', 'https://www.youtube.com/channel/UC0tRdbXVDbhaRvZPKsRgmxg', 'https://yt3.ggpht.com/a/AATXAJyjxOwl9VQSoBGpGnRg4pemzptdTDalfVwSRMN1Aw=s100-c-k-c0xffffffff-no-rj-mo'),
+(8, 'Thapa Technical', 'https://www.youtube.com/channel/UCwfaAHy4zQUb2APNOGXUCCA', 'https://yt3.ggpht.com/a/AATXAJwReHmC6QihHak4rs9IyN-i_PJw9U----fLrhWfHA=s100-c-k-c0xffffffff-no-rj-mo'),
+(9, 'Traversy Media', 'https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA', 'https://yt3.ggpht.com/a/AATXAJw6qBlNzbAweKz7UlC44hYLoEtdoXGmzN8IJno3mg=s100-c-k-c0xffffffff-no-rj-mo'),
+(10, 'Kevin Powel', 'https://www.youtube.com/channel/UCJZv4d5rbIKd4QHMPkcABCw', 'https://yt3.ggpht.com/a/AATXAJwEYMLm54q5CX7NwBsUrETHMZvMca9tp-3vxrorBA=s100-c-k-c0xffffffff-no-rj-mo'),
+(11, 'Dani Krossing', 'https://www.youtube.com/channel/UCzyuZJ8zZ-Lhfnz41DG5qLw', 'https://yt3.ggpht.com/a/AATXAJx6TRb2UtlFRUrc1y8izsVdh5KgI1kYYBdZTwigIQ=s100-c-k-c0xffffffff-no-rj-mo'),
+(12, 'Celever Programmer', 'https://www.youtube.com/channel/UCqrILQNl5Ed9Dz6CGMyvMTQ', 'https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s100-c-k-c0xffffffff-no-rj-mo'),
+(13, 'Easy Tutorials', 'https://www.youtube.com/channel/UCkjoHfkLEy7ZT4bA2myJ8xA', 'https://yt3.ggpht.com/a/AATXAJz3SEKxDump1_WJK18Zj6KN4F66XMuweqrmJGuhOw=s100-c-k-c0xffffffff-no-rj-mo'),
+(14, 'The Net Ninja', 'https://www.youtube.com/channel/UCW5YeuERMmlnqo4oq8vwUpg', 'https://yt3.ggpht.com/a/AATXAJyWnmk6Ql70o3P-x2eHIR01SiiYamhspnAN04zhWA=s100-c-k-c0xffffffff-no-rj-mo'),
+(15, 'Daily Tution', 'https://www.youtube.com/channel/UCrG2Z0usOCCdUTAr4D1A8mw', 'https://yt3.ggpht.com/a/AATXAJxezKduumrqYcY85c-cQI0os2Bo14y3xc0QmnFR_A=s100-c-k-c0xffffffff-no-rj-mo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating`
+--
+
+CREATE TABLE `rating` (
+  `channel_id` int(11) NOT NULL,
+  `rating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `rating`
+--
+
+INSERT INTO `rating` (`channel_id`, `rating`) VALUES
+(15, 5),
+(15, 3),
+(12, 3),
+(12, 5),
+(11, 1),
+(11, 4),
+(3, 4),
+(3, 3),
+(13, 5),
+(13, 3),
+(1, 2),
+(1, 4),
+(4, 1),
+(4, 2),
+(10, 4),
+(10, 3),
+(2, 5),
+(2, 1),
+(5, 2),
+(5, 5),
+(8, 5),
+(8, 2),
+(14, 1),
+(14, 2),
+(9, 2),
+(9, 4),
+(6, 2),
+(6, 4),
+(7, 2),
+(7, 5),
+(2, 4),
+(2, 4),
+(2, 4),
+(2, 4),
+(2, 4),
+(2, 4),
+(2, 4),
+(2, 4),
+(2, 4),
+(2, 4),
+(2, 4),
+(2, 4),
+(2, 4);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `channel`
+--
+ALTER TABLE `channel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rating`
+--
+ALTER TABLE `rating`
+  ADD KEY `channel_id` (`channel_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `channel`
+--
+ALTER TABLE `channel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `rating`
+--
+ALTER TABLE `rating`
+  ADD CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`channel_id`) REFERENCES `channel` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
